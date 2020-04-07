de42dcddb10cf7bc8276fd7609f0c581b6da852e
Pranay Ankit <pranayankit414@gmail.com>
2020-04-07 14:24:35 +0530

    add my aliases

---
+++ bash_profile.sh
@@ -0,0 +1,14 @@
+# My system aliases
+alias github='cd ~/Desktop/Github'
+
+# My conda aliases
+alias dconda='conda deactivate'
+alias aconda='conda activate'
+
+# My Git aliases
+alias init='git init'
+alias push='git push'
+alias clone='git clone'
+alias addrmo='git remote add origin'
+alias addrmup='git remote add upstream'
+alias hardreset='git reset --hard HEAD'
